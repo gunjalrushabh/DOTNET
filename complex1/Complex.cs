8d2f17e98e2cb945ae91b5117aa16bb8af185038
dac <dac@dypm.org>
2023-01-12 17:42:51 +0530

    updated Assignments

---
+++ complex1/Complex.cs
@@ -0,0 +1,57 @@
+namespace Maths;
+public class Complex {
+    private int real;
+    private int imag;
+
+    public Complex(){
+        this.real=0;
+        this.imag=0;
+    }
+    public Complex(int r, int i){
+        this.real=r;
+        this.imag=i;
+    }
+
+    //Property
+    public int Real{
+        get{return this.real;}
+        set{this.real=value;} 
+    }
+
+    public int Imag{
+        get{return this.imag;}
+        set{this.imag=value;}
+    }
+  
+  public static Complex operator+ (Complex c1, Complex c2)
+  {
+Complex temp=new Complex();
+temp.Real=c1.Real+c2.Real;
+temp.Imag=c1.Imag+c2.Imag;
+return temp;
+  }
+
+public static Complex operator- (Complex c1,Complex c2)
+{
+Complex temp=new Complex();
+temp.Real=c1.Real-c2.Real;
+temp.Imag=c1.Imag-c2.Imag;
+return temp;
+}
+
+public static Complex operator* (Complex c1,Complex c2)
+{
+    Complex temp =new Complex();
+    temp.Real=c1.Real*c2.Real;
+    temp.Imag=c1.Imag*c2.Imag;
+    return temp;
+}
+ public static Complex operator/ (Complex c1, Complex c2)
+ {
+    Complex temp= new Complex();
+    temp.Real=c1.Real/c2.Real;
+    temp.Imag=c1.Imag/c2.Imag;
+    return temp;
+ }
+
+}
---
+++ complex1/Program.cs
@@ -0,0 +1,14 @@
+﻿using Maths;
+Complex c1= new Complex(10,20);
+c1.Real=30;//setter
+c1.Imag=40;
+Console.WriteLine(c1.Real);//getter
+Complex c2= new Complex(20,30);
+Complex c3=c1+c2;//(30,40),,,,(20,30)
+Console.WriteLine("ADDITION Complex c3 real: "+c3.Real+" imag: "+c3.Imag);//50,70
+Complex c4=c1-c2;
+Console.WriteLine("SUBTRACTION Complex c4 real: "+c4.Real+ " imag: "+c4.Imag);
+Complex c5=c1 * c2;
+Console.WriteLine("Multiplication: "+c5.Real+" + i"+c5.Imag);
+Complex c6=c1/c2;
+Console.WriteLine("Divide: "+c6.Real+"  "+c6.Imag);
